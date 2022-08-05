2d098672374f96dc4908235aa58195b50867a653
matheusvasconcellos <matheusvclls97@gmail.com>
2022-08-04 22:55:38 -0300

    feat: add content to CDB function

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
 import math
 
-# # Juros compostos .
-#
-# É a adi ç ã o de juros ao capital principal de um empr é stimo ou dep ó sito ,
-# ou em outras palavras , juros sobre juros .
-#
-# É o resultado do reinvestimento dos juros , ao inv é s de pag á -lo ,
-# de tal forma que a taxa no pr ó ximo per í odo é calculada
-# sobre o principal , mais os juros recebidos previamente .
-#
-# A fun ç ã o de acumula ç ã o mostra como uma unidade monet á ria
-# cresce ap ó s o per í odo de tempo .
-#
-# @param r taxa de juros nominal .
-# @param t per í odo de tempo total no qual os juros s ã o aplicados
-# ( expressa nas mesmas unidades de tempo de r , usualmente anos ).
-# @param n frequ ê ncia de composi ç ã o ( pagamento dos juros ) , por exemplo ,
-# mensal , trimestral ou anual .
-# @return juros obtidos no per í odo : (1 + r/n) nt − 1
-#
-
 TAXA_SELIC_ANUAL = 0.1325
 TAXA_CDI_ANUAL = 0.1315
 
@@ -79,31 +59,61 @@ def doublePrincipal ( r : float ) -> float :
     return math.log(2,10)/math.log(1+r,10)
 #... Inclua suas fun ç õ es aqui ...
 
-# # Calcula o montante final , imposto , rendimento e
-# rentabilidade equivalente .
-#
-# @param c capital
-# @param cdi taxa cdi anual
-# @param p taxa poupan ç a anual = 0.70 * selic
-# @param t rentabilidade da aplica ç ã o em fun ç ã o do CDI
-# @param i al í quota do imposto de renda
-# @param m meses
-# @return
-# - montante da aplica ç ão ,
-# - montante poupan ça ,
-# - imposto de renda retido ,
-# - rendimento em m meses (%) ,
-# - rendimento em m meses ,
-# - rendimento l í quido em 1 m ês ,
-# - rentabilidade para igualar poupan ç a (%) CDI
-#
+
 def CDB ( c : float , cdi : float , p : float , t : float , i : float , m : int = 1) -> float :
+    '''
+    Função responsável por calcular o  montante final , imposto , rendimento e
+    rentabilidade equivalente da poupança e do CDI
+
+    Args:
+        c (float): capital investido
+        cdi (float): taxa cdi anual
+        p (float): taxa da poupança anual que é igual a 0.70 * selic
+        t (float): rentabilidade da aplicação em função do CDI
+        i (float): alíquota do imposto de renda
+        m (int): meses
+
+    Returns:
+        montante da aplicação
+        montante poupança
+        imposto de renda retido
+        rendimento em m meses (%)
+        rendimento em m meses
+        rendimento líquido em 1 mês
+        rentabilidade para igualar poupança (%) CDI
+    '''
+
+    taxa_cdi_mensal = round(year2month(cdi),4)
+    taxa_cdi_diaria = "ainda falta implementar"
+    taxa_poupanca_anual = p*.7
+    taxa_poupanca_mensal = round(year2month(taxa_poupanca_anual),4)
+
+    print("\n")
+    print("Capital = ${:.2f}".format(c))
+
+    print("Taxa SELIC = {}%".format(p*100))
+
+    print("Taxa CDI = {}% ao ano = {}% ao mês = {}% ao dia".format(cdi*100, taxa_cdi_mensal, taxa_cdi_diaria))
+    
+    # print com erro -> verificar
+    print("Taxa Poup = {}% ao ano = {}% ao mês".format(taxa_poupanca_anual*100,taxa_poupanca_mensal))
+    
+
+    print("\n")
+    print("IR = {}%".format(i))
+    print("\n")
+
+    rentabilidade_percentual_cdi = round(t*cdi,2)
+    rentabilidade_liquida_cdi = round(t*((100-i)/100),2)
+    rentabilidade_liquida_percentual_cdi = round(rentabilidade_percentual_cdi*((100-i)/100),2)
+
+    print("Rentabilidade = {:.1f}% CDI = {}%".format(t, rentabilidade_percentual_cdi))
+    print("Com impostos = {}% CDI = {}%".format(rentabilidade_liquida_cdi,rentabilidade_liquida_percentual_cdi))
+    
+    print("\n")
+    print("Meses = {}".format(m))
+
+    print("\n")
     return None
-#... Essa deve ser implementada por voc ê ... 
 
-print(jc(0.1,24,12))
-
-print(year2month ( 0.12 ))
-print(day2year ( 0.00049037 ))
-
-
+CDB(1000, 0.1315, 0.1325, 93, 22.5)
