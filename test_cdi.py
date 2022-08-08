f4f4902fa9232717741f802bfe1b3cbda27fc6f1
matheusvasconcellos <matheusvclls97@gmail.com>
2022-08-08 19:19:44 -0300

    feat: add tests to year2month and day2year functions

--- test_cdi.py
+++ test_cdi.py
@@ -7,5 +7,11 @@ class TestCdi(unittest.TestCase):
         self.assertAlmostEqual(cdi.calculo_rendimento_poupanca(0.086), 0.0617)
         self.assertAlmostEqual(cdi.calculo_rendimento_poupanca(0.05), 0.05*.7)
 
+    def test_year2month(self):
+        self.assertAlmostEqual(cdi.year2month(0.12),0.94887929)
+
+    def test_day2year(self):
+        self.assertAlmostEqual(cdi.day2year(0.000657),18,2)
+    
 if __name__ == "__main__":
     unittest.main()
