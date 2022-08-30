a4d4d96bde414981425a9f7dbf23d4035f001efe
matheusvasconcellos <matheusvclls97@gmail.com>
2022-08-30 08:04:02 -0300

    feat: add unittest to jc function

--- test_cdi.py
+++ test_cdi.py
@@ -13,5 +13,9 @@ class TestCdi(unittest.TestCase):
     def test_day2year(self):
         self.assertAlmostEqual(cdi.day2year(0.000657),18,2)
     
+    def test_jc(self):
+        self.assertAlmostEqual(cdi.jc(0.1,1,1),0.1,2)
+        self.assertAlmostEqual(cdi.jc(0.005,10,1),0.0511401,2)
+
 if __name__ == "__main__":
     unittest.main()
