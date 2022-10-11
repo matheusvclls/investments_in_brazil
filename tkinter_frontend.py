ba40a7438caf3d9f58a0059ee15786c300d8855e
Matheus Vasconcellos <matheusvclls97@gmail.com>
2022-10-11 00:43:48 -0300

    feat: receive data from backend

---
+++ tkinter_frontend.py
@@ -0,0 +1,140 @@
+from tkinter import *
+import tkinter.ttk as ttk
+from turtle import left
+import tkinter.font as font
+from cdi import CDB
+
+def get_aliquota_ir():
+    selected =  str(radio.get())
+    if selected == '0':
+        aliquota_ir=0
+    elif selected == '1':
+        aliquota_ir=15
+    elif selected == '2':
+        aliquota_ir=17.5
+    elif selected == '3':
+        aliquota_ir=20
+    elif selected == '4':
+        aliquota_ir=22.5
+    return aliquota_ir
+
+def call_function():
+    capital_value = float(capital.get())
+    tx_selic_value = float(tx_selic.get())
+    tx_cdi_value = float(tx_cdi.get())
+    rentabilidade_value = float(rentabilidade.get())
+    meses_value = float(meses.get())
+    aliquota_ir_value = get_aliquota_ir()
+
+    #CDB(capital_value, tx_cdi_value,tx_selic_value,rentabilidade_value,aliquota_ir_value,meses_value)
+
+    root = Tk()
+    framedisp = Frame(root)
+    border_color = Frame(framedisp, background="aquamarine")
+    lab = Label(border_color, text=CDB(capital_value, tx_cdi_value,tx_selic_value,rentabilidade_value,aliquota_ir_value,meses_value), anchor='w', font='Helvetica 11 bold')
+    lab.configure(background='red')
+    border_color.pack(padx=40, pady=20)
+    lab.pack(padx=5, pady=5,side = TOP)
+    framedisp.pack(side=TOP)
+
+
+if __name__ == '__main__':
+    capital = ''
+    tx_cdi = ''
+    tx_selic=''
+    rentabilidade=''
+    meses=''
+    radio=''
+    root = Tk()
+    
+    # Title
+    framedisp = Frame(root)
+    border_color = Frame(framedisp, background="aquamarine")
+    lab = Label(border_color, text="CDBs, LCIs e LCAs indexadas por \nCertificados de Depósitos Interbancários", anchor='w', font='Helvetica 11 bold')
+    lab.configure(background='red')
+    border_color.pack(padx=40, pady=20)
+    lab.pack(padx=5, pady=5,side = TOP)
+    framedisp.pack(side=TOP)
+
+
+    # Capital
+    framedisp = Frame(root)
+    framedisp.configure(height=20, width=20)    
+    w = Label(framedisp, width=22,text ='Capital', anchor='w') 
+    w.pack(side = LEFT)
+    capital = Spinbox(framedisp, from_= 0, increment=0.01)
+    capital.pack(side = RIGHT, expand = YES, fill = X)
+    framedisp.pack(side=TOP,  anchor="w",fill = X, padx = 5 , pady = 5)
+    
+    # Taxa Selic
+    framedisp = Frame(root)
+    framedisp.configure(height=20, width=20)    
+    w = Label(framedisp, width=22,text ='Taxa Selic', anchor='w') 
+    w.pack(side = LEFT)
+    tx_selic = Spinbox(framedisp, from_= 0, increment=0.01)
+    tx_selic.pack(side = RIGHT, expand = YES, fill = X)
+    framedisp.pack(side=TOP,  anchor="w",fill = X, padx = 5 , pady = 5)
+    
+    # Taxa CDI
+    framedisp = Frame(root)
+    framedisp.configure(height=20, width=20)    
+    w = Label(framedisp, width=22,text ='Taxa CDI', anchor='w') 
+    w.pack(side = LEFT)
+    tx_cdi = Spinbox(framedisp, from_= 0,  increment=0.01)
+    tx_cdi.pack(side = RIGHT, expand = YES, fill = X)
+    framedisp.pack(side=TOP,  anchor="w",fill = X, padx = 5 , pady = 5)
+    
+    # Rentabilidade
+    framedisp = Frame(root)
+    framedisp.configure(height=20, width=20)    
+    w = Label(framedisp, width=22,text ='Rentabilidade', anchor='w') 
+    w.pack(side = LEFT)
+    rentabilidade = Spinbox(framedisp, from_= 0,  increment=0.01)
+    rentabilidade.pack(side = RIGHT, expand = YES, fill = X)
+    framedisp.pack(side=TOP,  anchor="w",fill = X, padx = 5 , pady = 5)
+    
+    # Meses
+    framedisp = Frame(root)
+    framedisp.configure(height=20, width=20)    
+    w = Label(framedisp, width=22,text ='Meses', anchor='w') 
+    w.pack(side = LEFT)
+    meses = Spinbox(framedisp, from_= 0)
+    meses.pack(side = RIGHT, expand = YES, fill = X)
+    framedisp.pack(side=TOP,  anchor="w",fill = X, padx = 5 , pady = 5)
+    
+    # Alíquota IR text
+    framedisp = Frame(root)
+    framedisp.configure(height=20, width=20)
+    lab = Label(framedisp)
+    lab.configure(text=("Alíquota IR"))
+    lab.pack(side = LEFT)
+    framedisp.pack(side=TOP,  anchor="w")
+
+    frame1 = Frame(None)
+    frame1.configure(height=200, width=200)
+    radio = IntVar()
+    radiobutton11 = Radiobutton(frame1)
+    radiobutton11.configure(text=("0.0 (LCA ou LCI)"), variable=radio, value=0)
+    radiobutton11.pack(side="top",  anchor="w")
+    radiobutton12 = Radiobutton(frame1)
+    radiobutton12.configure(text=("15.0 (acima de 721 dias)"), variable=radio, value=1)
+    radiobutton12.pack(side="top",  anchor="w")
+    radiobutton13 =Radiobutton(frame1)
+    radiobutton13.configure(text=("17.5 (de 361 até 720 dias)"), variable=radio, value=2)
+    radiobutton13.pack(side="top",  anchor="w")
+    radiobutton14 = Radiobutton(frame1)
+    radiobutton14.configure(text=("20.0 (de 181 até 360 dias)"), variable=radio, value=3)
+    radiobutton14.pack(side="top",  anchor="w")
+    radiobutton15 = Radiobutton(frame1)
+    radiobutton15.configure(text=("22.5 (até 180 dias)"), variable=radio, value=4)
+    radiobutton15.pack(side="top",  anchor="w")
+    frame1.pack(side="top",  anchor="w")
+    
+    # "Calcular" button
+    b1 = Button(root, text = 'Calcular', command=call_function)
+    b1.pack(side = TOP, padx = 5, pady = 5)
+
+
+    root.mainloop()
+
+
